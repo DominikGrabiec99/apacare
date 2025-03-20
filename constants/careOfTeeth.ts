407d6d537c1193f89194752d671fc815886d45b7
Dominik Grabiec <dominik.grabiec@monaco.net.pl>
2025-03-20 20:17:00 +0100

    B: fix img import

--- constants/careOfTeeth.ts
+++ constants/careOfTeeth.ts
@@ -1,21 +1,21 @@
 export const CARE_OF_TEETH = [
   {
     id: 0,
-    photo: '/images/careOfTeeth/smile.png',
+    photo: './images/careOfTeeth/smile.png',
     title: 'There are many variations of passages of Lorem Ipsum available',
     text: 'There are many variations of passages of Lorem Ipsum available',
     link: '/',
   },
   {
     id: 1,
-    photo: '/images/careOfTeeth/health.png',
+    photo: './images/careOfTeeth/health.png',
     title: 'There are many variations of passages of Lorem Ipsum available',
     text: 'There are many variations of passages of Lorem Ipsum available',
     link: '/',
   },
   {
     id: 2,
-    photo: '/images/careOfTeeth/bacteria.png',
+    photo: './images/careOfTeeth/bacteria.png',
     title: 'There are many variations of passages of Lorem Ipsum available',
     text: 'There are many variations of passages of Lorem Ipsum available',
     link: '/',
--- constants/salesProductsAdvantages.ts
+++ constants/salesProductsAdvantages.ts
@@ -1,24 +1,24 @@
 export const SALES_PRODUCTS_ADVANTAGES = [
   {
-    photo: '/images/advantages/develop-by-dentists.svg',
+    photo: './images/advantages/develop-by-dentists.svg',
     icon: 'guidance:medical-laboratory',
     title: 'Opracowane przez dentystów',
     text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration `,
   },
   {
-    photo: '/images/advantages/recomended-stiftung.svg',
+    photo: './images/advantages/recomended-stiftung.svg',
     icon: 'solar:stars-minimalistic-outline',
     title: 'Jakość potwierdzona przez Stiftung Warentest',
     text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration `,
   },
   {
-    photo: '/images/advantages/liquid-enamel.svg',
+    photo: './images/advantages/liquid-enamel.svg',
     icon: 'hugeicons:dental-tooth',
     title: 'Płynne szkliwo robi różnice',
     text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration `,
   },
   {
-    photo: '/images/advantages/liquid-enamel.svg',
+    photo: './images/advantages/liquid-enamel.svg',
     icon: 'solar:test-tube-line-duotone',
     title: 'Potwierdzone badaniami',
     text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration `,

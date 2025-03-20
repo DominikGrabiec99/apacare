b2ecf63aeb06ebf0cce8fb6038d9043527596eda
Dominik Grabiec <dominik.grabiec@monaco.net.pl>
2025-03-20 20:13:59 +0100

    B: fix about us section style

--- components/molecule/AboutUs.vue
+++ components/molecule/AboutUs.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <h3 class="text-2xl font-bold text-primary-black md:text-4xl">O nas</h3>
-    <div class="mt-8 flex flex-col items-center gap-4 md:flex-row md:gap-12">
+    <div
+      class="mt-8 flex flex-col items-center gap-4 rounded-lg bg-secondary p-4 md:flex-row md:gap-12 md:p-8"
+    >
       <img
         src="/images/about-company.png"
         alt="zdjęcie firmy"
-        class="rounded-lg md:max-w-[300px]"
+        class="rounded-lg md:max-w-[400px]"
       />
-      <!-- <div class="rounded-lg bg-secondary p-4 md:p-6"> -->
       <p class="text-primary-black">{{ ABOUT_US_TEXT }}</p>
-      <!-- </div> -->
     </div>
   </div>
 </template>

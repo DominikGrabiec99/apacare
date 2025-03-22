40cd5c66d0ea8718f6acbf0dccb511439af3c0a0
Dominik Grabiec <dominik.grabiec@monaco.net.pl>
2025-03-22 12:51:03 +0100

    T: add A/B test to main section

--- components/atom/UnderlineNuxtLink.vue
+++ components/atom/UnderlineNuxtLink.vue
@@ -6,13 +6,14 @@
     >
       <span
         :class="[textClass]"
-        class="relative text-lg font-medium text-primary-black before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-full before:scale-x-100 before:bg-primary-black before:opacity-100 before:transition-all before:duration-700 before:ease-in-out before:content-[''] after:absolute after:bottom-0 after:left-1/2 after:h-[2px] after:w-0 after:bg-primary-black after:transition-all after:delay-300 after:duration-700 after:ease-in-out after:content-[''] hover:text-primary-black hover:before:scale-x-0 hover:before:opacity-0 hover:after:left-0 hover:after:w-full"
+        class="relative text-lg font-medium text-primary-black before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-full before:scale-x-100 before:bg-primary-black before:opacity-100 before:transition-all before:duration-700 before:ease-in-out before:content-[''] after:absolute after:bottom-0 after:left-1/2 after:h-[2px] after:w-0 after:bg-primary-black after:transition-all after:delay-300 after:duration-700 after:ease-in-out after:content-[''] hover:before:scale-x-0 hover:before:opacity-0 hover:after:left-0 hover:after:w-full"
       >
         {{ text }}
       </span>
       <NuxtIcon
         v-if="showIcon"
         :name="icon"
+        :class="[iconClass]"
         class="whitespace-nowrap text-sm font-semibold"
       />
     </div>
@@ -38,6 +39,10 @@ defineProps({
     default: '',
   },
   textClass: {
+    type: String,
+    default: '',
+  },
+  iconClass: {
     type: String,
     default: '',
   },
--- components/organism/MainInformation.vue
+++ components/organism/MainInformation.vue
@@ -1,5 +1,81 @@
 <template>
   <div
+    v-if="testAB"
+    class="md: relative min-h-[650px] overflow-hidden bg-cover bg-center pt-16 md:flex md:min-h-[800px] md:items-center md:bg-[url(./images/bg-main-section.png)] md:pt-24 md:before:absolute md:before:inset-0 md:before:bg-black md:before:bg-opacity-20"
+  >
+    <div
+      class="container flex h-full flex-col gap-5 px-6 md:h-min md:flex-row-reverse md:items-center md:justify-end md:gap-12 md:px-14 lg:gap-36"
+    >
+      <div
+        class="relative flex h-[22rem-16px] flex-shrink-0 items-center justify-end overflow-hidden pt-1 md:hidden md:h-[30rem] md:px-5"
+      >
+        <div
+          class="relative flex size-[calc(20rem)] -rotate-[25deg] items-center justify-center rounded-full border-2 border-solid border-gray-900 border-t-transparent before:absolute before:left-11 before:top-10 before:z-10 before:size-2 before:rounded-full before:bg-gray-900 after:absolute after:right-11 after:top-10 after:z-10 after:size-2 after:rounded-full after:bg-gray-900 md:size-[28rem] md:before:left-[60px] md:before:top-[60px] md:after:right-[60px] md:after:top-[60px]"
+        >
+          <div
+            class="relative size-72 rotate-[25deg] overflow-hidden rounded-full border-8 border-solid border-primary shadow-lg transition-transform duration-300 ease-in-out hover:scale-105 md:size-[calc(400px-20px)] md:border-[12px]"
+          >
+            <img
+              src="/images/header-main-page.png"
+              alt="Uśmiechnięta dziewczyna"
+              class="h-full w-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="relative h-full md:h-min">
+        <div class="pt-12">
+          <div class="flex flex-col gap-2">
+            <h1
+              class="text-5xl font-extrabold leading-tight text-primary-black md:text-white md:[text-shadow:_0_4px_8px_rgba(0,0,0,0.7)]"
+            >
+              Dbaj o swój
+            </h1>
+            <h2
+              class="text-3xl font-semibold text-gray-800 md:text-4xl md:text-white md:[text-shadow:_0_4px_8px_rgba(0,0,0,0.7)]"
+            >
+              codzienny uśmiech
+            </h2>
+          </div>
+          <div class="mt-6 flex max-w-80 flex-col gap-6 md:max-w-96">
+            <p
+              class="text-lg font-medium text-gray-700 md:text-white md:[text-shadow:_0_4px_8px_rgba(0,0,0,0.7)]"
+            >
+              Sprawdź nasze produkty, które zapewniają naturalnie gładkie,
+              zdrowe i lśniące zęby!
+            </p>
+            <!-- link="/produkty" -->
+            <AtomUnderlineNuxtLink
+              text="Sprawdź"
+              text-class="font-semibold md:text-white md:before:bg-white md:after:bg-white md:[text-shadow:_0_4px_8px_rgba(0,0,0,0.7)]"
+              icon-class="md:text-white md:[text-shadow:_0_4px_8px_rgba(0,0,0,0.7)]"
+              showIcon
+              @click="toggleSection"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Tło w kształcie koła -->
+    <div
+      class="absolute -right-32 bottom-10 size-72 rounded-full bg-primary opacity-10 md:hidden"
+    />
+
+    <!-- Ikonki ozdobne -->
+    <NuxtIcon
+      name="ri:tooth-line"
+      class="absolute left-6 top-6 text-[64px] text-primary opacity-10 md:hidden"
+    />
+    <NuxtIcon
+      name="ri:tooth-line"
+      class="absolute left-16 top-80 text-[58px] text-primary opacity-10 md:hidden"
+    />
+  </div>
+
+  <div
+    v-else
     class="relative min-h-[650px] overflow-hidden pt-16 md:flex md:min-h-[500px] md:items-center md:pt-24"
   >
     <div
@@ -38,11 +114,12 @@
               Sprawdź nasze produkty, które zapewniają naturalnie gładkie,
               zdrowe i lśniące zęby!
             </p>
+            <!-- link="/produkty" -->
             <AtomUnderlineNuxtLink
-              link="/produkty"
               text="Sprawdź"
               text-class="font-semibold"
               showIcon
+              @click="toggleSection"
             />
           </div>
         </div>
@@ -65,3 +142,11 @@
     />
   </div>
 </template>
+
+<script setup lang="ts">
+const testAB = ref(true);
+
+const toggleSection = () => {
+  testAB.value = !testAB.value;
+};
+</script>
